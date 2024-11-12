fb7e81fb67b663dd2a3137015019da200f69851d
jordan.hoover <jordan.hoover@toyotaconnected.com>
2024-11-11 20:20:34 -0600

    Finish cat setup

---
+++ README.md
@@ -0,0 +1,1 @@
+My version of simple command line apps in rust
--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// Rust version of `cat`
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Input files
+    files: Vec<String>,
+    #[arg(short, long)]
+    number_lines: bool,
+    #[arg(short = 'b', long)]
+    number_nonblank_lines: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{args:#?}");
 }
