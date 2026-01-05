87496028ddb4c0350fca2e585a1f888e5cc47ea1
jordan.hoover <jordan.hoover@toyotaconnected.com>
2026-01-05 13:39:54 -0600

    More wc setup

--- wc/src/main.rs
+++ wc/src/main.rs
@@ -1,3 +1,98 @@
+use anyhow::Result;
+use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+#[allow(dead_code)]
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// Input file(s)
+    #[arg(default_value = "-", value_name = "FILE")]
+    files: Vec<String>,
+
+    /// Show line count
+    #[arg(short, long)]
+    lines: bool,
+
+    /// Show word count
+    #[arg(short, long)]
+    words: bool,
+
+    /// Show byte count
+    #[arg(short('c'), long)]
+    bytes: bool,
+
+    /// Show character count
+    #[arg(short('m'), long, conflicts_with("bytes"))]
+    chars: bool,
+}
+
+#[derive(Debug, PartialEq)]
+struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if let Err(e) = run(args) {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
+
+fn run(args: Args) -> Result<()> {
+    for filename in &args.files {
+        match open(filename) {
+            Err(err) => eprintln!("{filename}: {err}"),
+                Ok(_) => println!("Opened {filename}"),
+        }
+    }
+
+    Ok(())
+}
+
+fn open(filename: &str) -> Result<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+fn count(mut file: impl BufRead) -> Result<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_bytes = 0;
+    let mut num_chars = 0;
+
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{count, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world.\nI just want your half.\r\n";
+        let info = count(Cursor::new(text));
+        assert!(info.is_ok());
+        let expected = FileInfo {
+            num_lines: 2,
+            num_words: 10,
+            num_chars: 48,
+            num_bytes: 48,
+        };
+        assert_eq!(info.unwrap(), expected);
+    }
 }
